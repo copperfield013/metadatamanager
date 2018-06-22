d712de5d78832ca58cbe72fb1c95fcd5490fd0e6
hbliu <hbliu@3051ac0c-2819-405f-8e91-413e4011bda0>
2018-06-22 02:58:17 +0000

    配置文件， swagger注解方法全部添加注解
    git-svn-id: http://47.100.187.235:13932/svn/metadatamanager/metadatamanager@193 3051ac0c-2819-405f-8e91-413e4011bda0

--- src/main/java/cn/sowell/datacenter/admin/controller/node/BasicItemNodeController.java
+++ src/main/java/cn/sowell/datacenter/admin/controller/node/BasicItemNodeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.abc.mapping.node.NodeOpsType;
 import com.abc.mapping.node.NodeType;
@@ -69,15 +70,20 @@ public class BasicItemNodeController {
 	@Resource
 	DictionaryBasicItemService dictBitemServices;
 	
-	@ApiIgnore
-	@ApiOperation(value="获取实体列表信息", notes="获取实体列表")
-	@RequestMapping("/list")
-	public String list(BasicItemNodeCriteria criteria, Model model, PageInfo pageInfo) {
+	 @ApiOperation(value = "获取实体列表信息", nickname = "list", notes = "获取实体列表", response = ModelAndView.class, tags={ "configurationFiles", })
+	    @ApiResponses(value = { 
+	        @ApiResponse(code = 200, message = "OK", response = ModelAndView.class),
+	        @ApiResponse(code = 401, message = "操作失败") })
+	    @RequestMapping(value = "/list",
+	        method = RequestMethod.POST)
+	public ModelAndView list(BasicItemNodeCriteria criteria, PageInfo pageInfo) {
 		List<BasicItemNode> list = basicItemNodeService.queryList(criteria, pageInfo);
-		model.addAttribute("list", list);
-		model.addAttribute("pageInfo", pageInfo);
-		model.addAttribute("criteria", criteria);
-		return AdminConstants.JSP_NODE + "/basicItemNode/list.jsp";
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("list", list);
+		modelAndView.addObject("pageInfo", pageInfo);
+		modelAndView.addObject("criteria", criteria);
+		modelAndView.setViewName(AdminConstants.JSP_NODE + "/basicItemNode/list.jsp");
+		return modelAndView;
 	}
 
 	@ResponseBody
@@ -123,44 +129,43 @@ public class BasicItemNodeController {
          }
 	}
 
-	@ApiIgnore
 	@ResponseBody
-	@RequestMapping("/do_delete")
-	public AjaxPageResponse doDelte(Integer id, boolean isDelChil) {
+	@ApiOperation(value = "删除节点", nickname = "doDelte", notes = "doDelte", tags={ "configurationFiles", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 401, message = "操作失败") })
+    @RequestMapping(value = "/do_delete",
+        method = RequestMethod.POST)
+	public ResponseEntity<AjaxPageResponse> doDelte(Integer id, boolean isDelChil) {
 		try {
 			basicItemNodeService.delete(id, isDelChil);
-			return AjaxPageResponse.REFRESH_LOCAL("删除成功");
+			 return new ResponseEntity<AjaxPageResponse>(AjaxPageResponse.REFRESH_LOCAL("删除成功"), HttpStatus.OK);
 		} catch (Exception e) {
-			return AjaxPageResponse.FAILD("删除失败");
+			return new ResponseEntity<AjaxPageResponse>(AjaxPageResponse.FAILD("删除失败"),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
-	/*public String entityList() {
-		BasicItemCriteria criteria = new BasicItemCriteria();
-		criteria.setDataType("记录类型");
-		criteria.setUsingState(1);
-		List<BasicItem> list = basicItemService.queryList(criteria);
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("entity", list);
-		JSONObject jobj = new JSONObject(map);
-		return jobj.toString();
-	}*/
-
 	// ajax 获取NodeOpsType
-	@ApiIgnore
-	@ApiOperation(notes = "getNodeOpsType", httpMethod = "POST", value = "添加一个新的群组")
 	@ResponseBody
-	@RequestMapping("/getNodeOpsType")
-	public String getNodeOpsType() {
-		List list = new ArrayList();
-
-		for (NodeOpsType f : NodeOpsType.values()) {
-			list.add(f.getName());
-		}
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("nodeOpsType", list);
-		JSONObject jobj = new JSONObject(map);
-		return jobj.toString();
+	@ApiOperation(value = "获取NodeOpsType", nickname = "getNodeOpsType", notes = "getNodeOpsType", response = Object.class, tags={ "configurationFiles", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK", response = Object.class),
+        @ApiResponse(code = 401, message = "操作失败") })
+    @RequestMapping(value = "/getNodeOpsType",
+        method = RequestMethod.POST)
+	public ResponseEntity<Object> getNodeOpsType() {
+		 try {
+			 	List list = new ArrayList();
+				for (NodeOpsType f : NodeOpsType.values()) {
+					list.add(f.getName());
+				}
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("nodeOpsType", list);
+				JSONObject jobj = new JSONObject(map);
+			 return new ResponseEntity<Object>(jobj, HttpStatus.OK);
+         } catch (Exception e) {
+             return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
+         }
 	}
 
 	// ajax 获取dataType
@@ -172,20 +177,23 @@ public class BasicItemNodeController {
     @RequestMapping(value = "/getDataType",
         method = RequestMethod.POST)
 	public ResponseEntity<Object> getDataType() {
-		List<String> list = new ArrayList<String>();
-		list.add(ValueTypeConstant.ABCT_NAME_BYTES);
-		list.add(ValueTypeConstant.ABCT_NAME_DATE);
-		list.add(ValueTypeConstant.ABCT_NAME_DATETIME);
-		list.add(ValueTypeConstant.ABCT_NAME_DOUBLE);
-		list.add(ValueTypeConstant.ABCT_NAME_FLOAT);
-		list.add(ValueTypeConstant.ABCT_NAME_INT);
-		list.add(ValueTypeConstant.ABCT_NAME_LONG);
-		list.add(ValueTypeConstant.ABCT_NAME_STRING);
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("dataType", list);
-		JSONObject jobj = new JSONObject(map);
-		
-		return new ResponseEntity<Object>(jobj, HttpStatus.OK);
+		try {
+			List<String> list = new ArrayList<String>();
+			list.add(ValueTypeConstant.ABCT_NAME_BYTES);
+			list.add(ValueTypeConstant.ABCT_NAME_DATE);
+			list.add(ValueTypeConstant.ABCT_NAME_DATETIME);
+			list.add(ValueTypeConstant.ABCT_NAME_DOUBLE);
+			list.add(ValueTypeConstant.ABCT_NAME_FLOAT);
+			list.add(ValueTypeConstant.ABCT_NAME_INT);
+			list.add(ValueTypeConstant.ABCT_NAME_LONG);
+			list.add(ValueTypeConstant.ABCT_NAME_STRING);
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("dataType", list);
+			JSONObject jobj = new JSONObject(map);
+			return new ResponseEntity<Object>(jobj, HttpStatus.OK);
+        } catch (Exception e) {                
+            return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 
 	// ajax 根据实体id, 获取本实体下所有的多值属性本身
@@ -197,15 +205,19 @@ public class BasicItemNodeController {
     @RequestMapping(value = "/getRepeat",
         method = RequestMethod.POST)
 	public ResponseEntity<InlineResponse2002> getRepeat(String entityId) {
-		BasicItemCriteria criteria = new BasicItemCriteria();
-		criteria.setParent(entityId);
-		criteria.setDataType("重复类型");
-		criteria.setUsingState(1);
+		try {
+			BasicItemCriteria criteria = new BasicItemCriteria();
+			criteria.setParent(entityId);
+			criteria.setDataType("重复类型");
+			criteria.setUsingState(1);
 
-		List<BasicItem> list = basicItemService.queryList(criteria);
-		InlineResponse2002 inline = new InlineResponse2002();
-		inline.repeat(list);
-		return new ResponseEntity<InlineResponse2002>(inline, HttpStatus.OK);
+			List<BasicItem> list = basicItemService.queryList(criteria);
+			InlineResponse2002 inline = new InlineResponse2002();
+			inline.repeat(list);
+			return new ResponseEntity<InlineResponse2002>(inline, HttpStatus.OK);
+        } catch (Exception e) {                
+            return new ResponseEntity<InlineResponse2002>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 
 	@ResponseBody
@@ -216,34 +228,39 @@ public class BasicItemNodeController {
     @RequestMapping(value = "/getRepeatChild",
         method = RequestMethod.POST)
 	public ResponseEntity<InlineResponse2001> getRepeatChild(String repeatId) {
-		BasicItem repeat = basicItemService.getBasicItem(repeatId);
-		BasicItemCriteria criteria = new BasicItemCriteria();
-		criteria.setParent(repeat.getParent() + "_" + repeatId);
-		criteria.setUsingState(1);
-		List<BasicItem> list = basicItemService.queryList(criteria);
-		InlineResponse2001 inline = new InlineResponse2001();
-		inline.repeatChild(list);
-        return new ResponseEntity<InlineResponse2001>(inline, HttpStatus.OK);
+        try {
+        	BasicItem repeat = basicItemService.getBasicItem(repeatId);
+    		BasicItemCriteria criteria = new BasicItemCriteria();
+    		criteria.setParent(repeat.getParent() + "_" + repeatId);
+    		criteria.setUsingState(1);
+    		List<BasicItem> list = basicItemService.queryList(criteria);
+    		InlineResponse2001 inline = new InlineResponse2001();
+    		inline.repeatChild(list);
+            return new ResponseEntity<InlineResponse2001>(inline, HttpStatus.OK);
+        } catch (Exception e) {                
+            return new ResponseEntity<InlineResponse2001>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 
-	//TODO.....
 	// ajax 根据实体id， 获取当前实体下的所有普通属性，
 	// 还有当前实体下的所有二级属性
-	@ApiIgnore
 	@ResponseBody
-	/* @ApiOperation(value = "根据实体id， 获取当前实体下的所有普通属性和二级属性", nickname = "getComm", notes = "根据实体id， 获取当前实体下的所有普通属性和二级属性", response = InlineResponse200.class, tags={ "configurationFiles", })
+	 @ApiOperation(value = "根据实体id， 获取当前实体下的所有普通属性和二级属性", nickname = "getComm", notes = "根据实体id， 获取当前实体下的所有普通属性和二级属性", response = Object.class, tags={ "configurationFiles", })
     @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "操作成功", response = InlineResponse200.class),
-        @ApiResponse(code = 400, message = "操作失败", response = String.class) })*/
-    @RequestMapping(value = "/getComm")
-	public String getComm(String entityId) {
-		List list = basicItemService.getComm(entityId);
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("comm", list);
-		JSONObject jobj = new JSONObject(map);
-		return jobj.toString();
+        @ApiResponse(code = 200, message = "操作成功", response = Object.class),
+        @ApiResponse(code = 400, message = "操作失败", response = String.class) })
+    @RequestMapping(value = "/getComm", method = RequestMethod.POST)
+	public ResponseEntity<Object> getComm(String entityId) {
+		try {
+			List list = basicItemService.getComm(entityId);
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("comm", list);
+			JSONObject jobj = new JSONObject(map);
+			return new ResponseEntity<Object>(jobj, HttpStatus.OK);
+        } catch (Exception e) {                
+            return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
-
 	
 	// 获取lab关系名称, 关系下边的lab
 	@ResponseBody
@@ -254,11 +271,15 @@ public class BasicItemNodeController {
     @RequestMapping(value = "/getLabRela",
         method = RequestMethod.POST)
 	public ResponseEntity<RecordRelationTypes> getLabRela(String leftRecordType, String rightRecordType) {
-		List<RecordRelationType> list = recordRelationTypeService.getEntityRelaByBitemId(leftRecordType,
-				rightRecordType);
-		RecordRelationTypes recordRelTypes = new RecordRelationTypes();
-		recordRelTypes.labRela(list);
-		return new ResponseEntity<RecordRelationTypes>(recordRelTypes, HttpStatus.OK);
+		try {
+			List<RecordRelationType> list = recordRelationTypeService.getEntityRelaByBitemId(leftRecordType,
+					rightRecordType);
+			RecordRelationTypes recordRelTypes = new RecordRelationTypes();
+			recordRelTypes.labRela(list);
+			return new ResponseEntity<RecordRelationTypes>(recordRelTypes, HttpStatus.OK);
+        } catch (Exception e) {                
+            return new ResponseEntity<RecordRelationTypes>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 
 	// 获取标签名称， 实体下面和属性组下边的标签， 从DictionaryBasicItem
@@ -270,34 +291,47 @@ public class BasicItemNodeController {
     @RequestMapping(value = "/getCommLab",
         method = RequestMethod.POST)
 	public ResponseEntity<DictionaryBasicItems> getCommLab() {
-		List<DictionaryBasicItem> list = dictBitemServices.getDictBasicItemByParent(125);
-		DictionaryBasicItems dictBasicItems = new DictionaryBasicItems();
-		dictBasicItems.commLab(list);
-		return new ResponseEntity<DictionaryBasicItems>(dictBasicItems, HttpStatus.OK);
+		try {
+			List<DictionaryBasicItem> list = dictBitemServices.getDictBasicItemByParent(125);
+			DictionaryBasicItems dictBasicItems = new DictionaryBasicItems();
+			dictBasicItems.commLab(list);
+			return new ResponseEntity<DictionaryBasicItems>(dictBasicItems, HttpStatus.OK);
+        } catch (Exception e) {                
+            return new ResponseEntity<DictionaryBasicItems>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 
-	//TODO.....
-	@ApiIgnore
-	@RequestMapping("/operate")
-	public String operate(Model model) {
+	@ApiOperation(value = "创建一个配置文件入口", nickname = "operate", notes = "创建一个配置文件入口", response = ModelAndView.class, tags={ "configurationFiles", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK", response = ModelAndView.class),
+        @ApiResponse(code = 401, message = "操作失败") })
+    @RequestMapping(value = "/operate",
+        method = RequestMethod.POST)
+	public ModelAndView operate() {
 		BasicItemCriteria criteria = new BasicItemCriteria();
 		criteria.setDataType("记录类型");
 		criteria.setUsingState(1);
 		List<BasicItem> list = basicItemService.queryList(criteria);
-		model.addAttribute("list", list);
-		return AdminConstants.JSP_NODE + "/basicItemNode/operate.jsp";
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("list", list);
+		mv.setViewName(AdminConstants.JSP_NODE + "/basicItemNode/operate.jsp");
+		return mv;
 	}
 	
-	//TODO.....
-	@ApiIgnore
-	@RequestMapping("/edit")
-	public String edit(String nodeId, Model model) {
+	@ApiOperation(value = "编辑配置文件入口", nickname = "edit", notes = "编辑配置文件入口", response = ModelAndView.class, tags={ "configurationFiles", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK", response = ModelAndView.class),
+        @ApiResponse(code = 401, message = "操作失败") })
+    @RequestMapping(value = "/edit",
+        method = RequestMethod.POST)
+	public ModelAndView edit(String nodeId) {
 		BasicItemNode btNode = basicItemNodeService.getOne(Integer.parseInt(nodeId));
 		BasicItem basicItem = basicItemService.getBasicItem(btNode.getAbcattrCode());
-		
-		model.addAttribute("btNode", btNode);
-		model.addAttribute("basicItem", basicItem);
-		return AdminConstants.JSP_NODE + "/basicItemNode/edit.jsp";
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("btNode", btNode);
+		mv.addObject("basicItem", basicItem);
+		mv.setViewName(AdminConstants.JSP_NODE + "/basicItemNode/edit.jsp");
+		return mv;
 	}
 	
 	@ResponseBody
@@ -308,10 +342,14 @@ public class BasicItemNodeController {
     @RequestMapping(value = "/getChildNode",
         method = RequestMethod.POST)
 	public ResponseEntity<BasicItemNodes> getChildNode(String nodeId) {
-		List<BasicItemNode> list = basicItemNodeService.getChildNode(nodeId);
-		BasicItemNodes btItemNodes = new BasicItemNodes();
-		btItemNodes.childNode(list);
-		return new ResponseEntity<BasicItemNodes>(btItemNodes, HttpStatus.OK);
+		try {
+			List<BasicItemNode> list = basicItemNodeService.getChildNode(nodeId);
+			BasicItemNodes btItemNodes = new BasicItemNodes();
+			btItemNodes.childNode(list);
+			return new ResponseEntity<BasicItemNodes>(btItemNodes, HttpStatus.OK);
+        } catch (Exception e) {                
+            return new ResponseEntity<BasicItemNodes>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 
 	@ResponseBody
@@ -340,10 +378,14 @@ public class BasicItemNodeController {
     @RequestMapping(value = "/entityList",
         method = {RequestMethod.POST})
 	public ResponseEntity<BasicItems> entityList(String leftRecordType) {
-		List<BasicItem> list = basicItemService.getEntityList(leftRecordType);
-		BasicItems btItems = new BasicItems();
-		btItems.entity(list);
-		return new ResponseEntity<BasicItems>(btItems, HttpStatus.OK);
+		try {
+			List<BasicItem> list = basicItemService.getEntityList(leftRecordType);
+			BasicItems btItems = new BasicItems();
+			btItems.entity(list);
+			return new ResponseEntity<BasicItems>(btItems, HttpStatus.OK);
+		} catch (Exception e) {
+            return new ResponseEntity<BasicItems>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 		
 }
