3e3940cabb82d4eefc3489abd42d74ed6e24d77f
hbliu <hbliu@3051ac0c-2819-405f-8e91-413e4011bda0>
2018-11-23 10:33:00 +0000

    git-svn-id: http://47.100.187.235:13932/svn/metadatamanager/metadatamanager@387 3051ac0c-2819-405f-8e91-413e4011bda0

--- src/main/java/cn/sowell/datacenter/model/dictionary/service/impl/BasicItemServiceImpl.java
+++ src/main/java/cn/sowell/datacenter/model/dictionary/service/impl/BasicItemServiceImpl.java
@@ -493,7 +493,7 @@ public class BasicItemServiceImpl implements BasicItemService {
 		
 		btItem.setCode(AttributeParter.getLeafEditTimeName(obj.getCode()));
 		btItem.getOneLevelItem().setCode(AttributeParter.getLeafEditTimeName(obj.getCode()));
-		btItem.setCnName(AttributeParter.getLeafEditTimeCNName(obj.getCnName()));
+		btItem.setCnName(AttributeParter.getLeafEditTimeCNName(""));
 		btItem.getOneLevelItem().setDataType(String.valueOf(ValueType.DATETIME.getIndex()));
 		btItem.getOneLevelItem().setDataRange(ValueType.DATETIME.getName());
 		btItem.getOneLevelItem().setTableName("t_" + obj.getCode() + "_m");
