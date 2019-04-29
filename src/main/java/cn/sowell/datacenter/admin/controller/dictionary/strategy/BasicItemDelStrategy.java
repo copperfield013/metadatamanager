3521dd6f3a45a21f1e076675ef97bcc47b076708
hbliu <hbliu@3051ac0c-2819-405f-8e91-413e4011bda0>
2019-04-29 09:36:55 +0000

    git-svn-id: http://47.100.187.235:13932/svn/metadatamanager/metadatamanager@504 3051ac0c-2819-405f-8e91-413e4011bda0

--- src/main/java/cn/sowell/datacenter/admin/controller/dictionary/strategy/BasicItemDelStrategy.java
+++
@@ -1,9 +0,0 @@
-package cn.sowell.datacenter.admin.controller.dictionary.strategy;
-
-import cn.sowell.datacenter.model.dictionary.dao.BasicItemDao;
-import cn.sowell.datacenter.model.dictionary.pojo.BasicItem;
-import cn.sowell.datacenter.model.dictionary.service.BiRefAttrService;
-
-public interface BasicItemDelStrategy {
-	public void delete(BasicItemDelContext basicItemDelContext, BasicItemDao basicItemDao, BiRefAttrService biRefAttrService, BasicItem basicItem);
-}
